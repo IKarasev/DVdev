a99f86eb7be6151bedac80f506fc069b3609ed44
IKarasev <iakarsev@gmail.com>
2016-09-01 23:01:24 +0300

    Added checks for part number and dates

--- checkutilyt-v01.py
+++ checkutilyt-v01.py
@@ -5,6 +5,7 @@ By Karasev I.A.
 
 import xlrd
 import xlwt
+import datetime
 from xlutils.copy import copy
 
 
@@ -41,6 +42,8 @@ PART_TYPE = {
 
 }
 
+DATE_FRAME = 3
+
 
 def main():
 	"""Функция выполняемая при запуске скрипта"""
@@ -57,16 +60,45 @@ def get_excel_file():
 	return wb
 
 
-def compare_row(util_row, svod_row):
+def compare_row(util_row, svod_row, svod):
 	"""Производит сравнение строк и возвращает True если критерии
 	удволетворены и False если нет"""
 
 	result = False
 
-	if util_row[UTIL_DATA['VAGON_NUMBER']].value == svod_row[SVOD_DATA['VAGON_NUMBER']].value:
-		result = True
+	"""Сравниваем номер вагона"""
+	if util_row[UTIL_DATA['VAGON_NUMBER']].value == \
+		svod_row[SVOD_DATA['VAGON_NUMBER']].value:
+
+		"""Номер вагона совпал, подготавливаем даты и сравниваем (СВОД-3 <= УТИЛИТА <= СВОД+3"""
+		util_date = text_to_date(util_row[UTIL_DATA["IN_DATE"]].value)
+		svod_date = xl_to_date(svod_row[SVOD_DATA["IN_DATE"]].value,svod)
+		margin = datetime.timedelta(days = DATE_FRAME)
+
+		if svod_date - margin <= util_date <= svod_date + margin:
+
+			"""Дата утилиты входит в период свода (-3,+3)
+				Проверяем есть ли номер детали в утилите
+			"""
+
 
 	return result
+
+"""******* РАБОТА С ДАТАМИ **********************"""
+
+def xl_to_date(cell_value,wb):
+	"""Преобразуем дату из excel в дату python"""
+	pydate = datetime.datetime(*xlrd.xldate_as_tuple(cell_value,wb.datemode))
+	return pydate
+
+def text_to_date(cell_value):
+	"""Преобразуем текст даты из Утилиты в дату python
+	   формат даты в ячейке 01/01/2014
+	"""
+	pydate = datetime.datetime.strptime(cell_value,"%d/%m/%Y")
+	return pydate
+
+"""**********************************************"""
 
 
 def analyse_files():
@@ -80,7 +112,7 @@ def analyse_files():
 	print("Зоздается таблица результатов")
 #	svod_result = copy(svod)
 
-	print("Введите путь для сохранения результата")
+#	print("Введите путь для сохранения результата")
 #	result_path = input(">:")
 
 	"""Получаем первые листа свода и утилиты"""
@@ -88,12 +120,13 @@ def analyse_files():
 	svod_sheet = svod.sheet_by_index(0)
 #	svod_result_sheet = svod_result.get_sheet(0)
 
-	res = compare_row(util_sheet.row(2),svod_sheet.row(3))
-
-	print(res)
-
 	"""Сохраняем результат"""
 #	svod_result.save("%sresult.xls"%(result_path))
 #	print("Результат сохранен в %s"%(result_path))
+
+	res = compare_row(util_sheet.row(UTIL_ROW_START), \
+			svod_sheet.row(SVOD_ROW_START), svod)
+
+	print(res)
 
 main()

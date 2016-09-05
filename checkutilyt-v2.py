823cdb7d49657ae35d45bb188d685f072fd1f500
IKarasev <iakarsev@gmail.com>
2016-09-05 20:41:19 +0300

    Complited v2

--- checkutilyt-v2.py
+++ checkutilyt-v2.py
@@ -68,70 +68,57 @@ def get_util_gradation(util_row):
 	return grad
 
 
-def compare_row(util_row, svod_row, svod):
+def compare_row(util_row, svod_row, svod_row_num, svod, util_cache):
 	"""Производит сравнение строк и возвращает True если критерии
 	удволетворены и False если нет"""
 
 	result = False
 
-#	print("--->(0) Сравнение началось")
+	if not svod_row[SVOD_DATA["COMMENT"]].value:
+		"""(1) Проверяем, есть ли уже записанная деталь"""
+		
+		if util_row[UTIL_DATA['VAGON_NUMBER']].value == svod_row[SVOD_DATA['VAGON_NUMBER']].value:
+			"""(2) Записи нет - проверяем номер вагона"""
 
-	"""(1) Сравниваем номер вагона"""
-	if util_row[UTIL_DATA['VAGON_NUMBER']].value == svod_row[SVOD_DATA['VAGON_NUMBER']].value:
+			
+			util_date = text_to_date(util_row[UTIL_DATA["IN_DATE"]].value)
+			svod_date = xl_to_date(svod_row[SVOD_DATA["IN_DATE"]].value,svod)
+			margin = datetime.timedelta(days = DATE_FRAME)
 
-#		print("--->(1) Номер вагона совпал")
+			if (svod_date - margin).date() < util_date.date() < (svod_date + margin).date():
+				"""(3) Номер вагона совпал - проверяем дату"""
 
-		"""(2) Номер вагона совпал, подготавливаем даты и сравниваем (СВОД-3 <= УТИЛИТА <= СВОД+3"""
-		util_date = text_to_date(util_row[UTIL_DATA["IN_DATE"]].value)
-		svod_date = xl_to_date(svod_row[SVOD_DATA["IN_DATE"]].value,svod)
-		margin = datetime.timedelta(days = DATE_FRAME)
+				if util_row[UTIL_DATA["PART_NUMBER"]].value:
+					"""(4) Дата совпала - проверяем наличие номера детали"""
 
-		if (svod_date - margin).date() < util_date.date() < (svod_date + margin).date():
-			"""(2) Дата утилиты входит в период свода (-3,+3)
-				Проверяем есть ли номер детали в утилите
-			"""
+					if int(util_row[UTIL_DATA["PART_NUMBER"]].value) == int(svod_row[SVOD_DATA["PART_NUMBER"]].value):
+						"""(5) Номер детали есть - сравниваем"""
 
-#			print("--->(2) Даты совпали")
-
-			if util_row[UTIL_DATA["PART_NUMBER"]].value:
-				"""(3) У утилиты есть номер детали"""
-
-#				print("--->(3) Номер детали есть")
-
-				"""(4) Сравниваем номера деталей в утилите и своде"""
-				if int(util_row[UTIL_DATA["PART_NUMBER"]].value) == int(svod_row[SVOD_DATA["PART_NUMBER"]].value):
-					"""Номер детали совпал - записи совпали"""
-					
-#					print("--->(4) Номер детали совпадает")
-					
-					result = True
-
-
-			elif PART_TYPE[util_row[UTIL_DATA["TYPE"]].value] == svod_row[SVOD_DATA["TYPE"]].value:
-				"""(5) У утилиты нет номера детали. Сравниваем тип детали"""
-#				print("--->(5) Тип детали совпал")
-
-				if util_row[UTIL_DATA["TYPE"]].value == "КП":
-					"""(6) Тип детали совподает. Проверяем, является ли деталь КП"""
-#					print("--->(6) Деталь является КП")
-
-					util_gradation = get_util_gradation(util_row)
-
-					"""(7)Деталь является КП, сравниваем градацию"""
-					if util_gradation[1] <= int(svod_row[SVOD_DATA["GRADATION"]].value) <= util_gradation[0]:
-						"""Если градация свода входит в градацию утилиты, то записываем результат"""
+						"""Номер детали совпал - записываем данные строку свода
+						и наличие номер детали в util_cache"""
+						util_cache.append({"svod_row_num": svod_row_num, "type": 1})
 						result = True
-#						print("--->(7) Градация совподает")
 
-					"""(8) Деталь не является КП, но тип детали сходится, проверяем, было ли схождение раньше"""
+				elif PART_TYPE[util_row[UTIL_DATA["TYPE"]].value] == svod_row[SVOD_DATA["TYPE"]].value:
+					"""(6) У утилиты нет номера детали. Сравниваем тип детали"""
 
-				elif not svod_row[SVOD_DATA["COMMENT"]].value:
-					"""Ранних схождений не найденно - записываем текущее схождение"""
-					result = True
+					if util_row[UTIL_DATA["TYPE"]].value == "КП":
+						"""(7) Тип детали совпа. Проверяем это колесная пара?"""
+						util_gradation = get_util_gradation(util_row)
 
-#					print("--->(7) НЕ КП")
-#					print("--->(8) Записи нет")
-
+						if util_gradation[1] <= int(svod_row[SVOD_DATA["GRADATION"]].value) <= util_gradation[0]:
+							"""(8) Тип детали является КП. Сравниваем Градацию"""
+							""" Градация совпала - записываем в КЭШ номер строки свода и градацию"""
+							util_cache.append({"svod_row_num": svod_row_num, "type": 2})
+							result = True
+						else:
+							"""Градация не совпала, записываем в КЭШ без типа"""
+							util_cache.append({"svod_row_num": svod_row_num, "type": 0})
+							result = True
+					else:
+						"""Тип детали не КП, но совпала - записываем в КЭШ без типа"""
+						util_cache.append({"svod_row_num": svod_row_num, "type": 0})
+						result = True
 
 	return result
 
@@ -150,7 +137,19 @@ def text_to_date(cell_value):
 	return pydate
 
 """**********************************************"""
-
+"""******* РАБОТА С КЭШЕМ УТИЛИТЫ****************"""
+def check_util_cashe(util_cache):
+	if util_cache:
+		for item in util_cache:
+			if item["type"] == 1:
+				return item["svod_row_num"]
+		for item in util_cache:
+			if item["type"] == 2:
+				return item["svod_row_num"]
+		return util_cache[0]["svod_row_num"]
+	else:
+		return 0
+"""**********************************************"""
 
 def analyse_files():
 	"""Проводит сравнительный анализ двух файлов и записывает результат
@@ -174,22 +173,26 @@ def analyse_files():
 	"""Проводим проверку по всем строкам"""
 	for util_row_num in range(UTIL_ROW_START, util_sheet.nrows):
 		print("Строка утилиты: %s"%(util_row_num+1))
-
+		util_cache = []
 		for svod_row_num in range(SVOD_ROW_START, svod_sheet.nrows):
 			try:
 				"""Проверка если запись совполает"""
-				comp_result = compare_row(util_sheet.row(util_row_num), svod_sheet.row(svod_row_num), svod)
-
-				"""Если схождение - записываем результат в итоговую таблицу"""
-				if comp_result:
-					"""Записываем наименование МЦ"""
-					svod_result_sheet.write(svod_row_num, SVOD_DATA["COMMENT"], util_sheet.cell(util_row_num,UTIL_DATA["NAME_MC"]).value)
-					"""Записываем сцеп"""
-					svod_result_sheet.write(svod_row_num, SVOD_DATA["SCEP"], util_sheet.cell(util_row_num,UTIL_DATA["SCEP"]).value)
-					"""Записываем соотвествующую строку утилиты"""
-					svod_result_sheet.write(svod_row_num, SVOD_DATA["UTIL_ROW"], str(util_row_num))
+				comp_result = compare_row(util_sheet.row(util_row_num), svod_sheet.row(svod_row_num), svod_row_num, svod, util_cache)
 			except Exception:
-				print("warning: utility-%s svod-%s"%(util_row_num, svod_row_num))
+				print("warnign: util-%s  svod-%s"%(util_row_num+1, svod_row_num+1))
+
+		if comp_result:
+			"""Если схождение - записываем результат в итоговую таблицу"""
+			print("util cache :   "+str(util_cache))
+			write_row_number = check_util_cashe(util_cache)
+			print(write_row_number)
+			"""Записываем наименование МЦ"""
+			svod_result_sheet.write(write_row_number, SVOD_DATA["COMMENT"], util_sheet.cell(util_row_num,UTIL_DATA["NAME_MC"]).value)
+			"""Записываем сцеп"""
+			svod_result_sheet.write(write_row_number, SVOD_DATA["SCEP"], util_sheet.cell(util_row_num,UTIL_DATA["SCEP"]).value)
+			"""Записываем соотвествующую строку утилиты"""
+			svod_result_sheet.write(write_row_number, SVOD_DATA["UTIL_ROW"], str(util_row_num))
+
 	"""Сохраняем результат"""
 	svod_result.save("%s\\result.xls"%(result_path))
 	print("Результат сохранен в %s"%(result_path))
@@ -197,5 +200,6 @@ def analyse_files():
 	""" ТЕСТЫ при разработке """
 #	res = compare_row(util_sheet.row(UTIL_ROW_START), \
 #			svod_sheet.row(SVOD_ROW_START), svod)
+
 
 main()
